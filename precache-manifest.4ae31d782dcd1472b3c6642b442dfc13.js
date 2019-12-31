a12243fdeb21a4315476a3bdfe919bbc859b8a5e
Isabel Jiang <isabeljiang@Isabels-MBP.socal.rr.com>
2019-12-31 14:08:41 -0800

    Updates

--- precache-manifest.4ae31d782dcd1472b3c6642b442dfc13.js
+++ precache-manifest.4ae31d782dcd1472b3c6642b442dfc13.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "937607cef02303954909ecd663c822cf",
+    "revision": "11847058efbaedc5aa2bb897e5c48f62",
     "url": "/portfolio/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.94d2d175.chunk.js.LICENSE"
   },
   {
-    "revision": "4644a2d953ea9ed02503",
-    "url": "/portfolio/static/js/main.9ec09a7d.chunk.js"
+    "revision": "75295a1de8e354529476",
+    "url": "/portfolio/static/js/main.68a2456e.chunk.js"
   },
   {
     "revision": "77eba9e9168a8673e11d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.5882e9d4ce322a29443c99fbd120ca9f.js"
+  "/portfolio/precache-manifest.4ae31d782dcd1472b3c6642b442dfc13.js"
 );
 
 self.addEventListener('message', (event) => {
