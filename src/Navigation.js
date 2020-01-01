49d6b3e0b6ea88171aff245d803d6b9ded2fbc0a
Isabel Jiang <isabeljiang@Isabels-MBP.socal.rr.com>
2019-12-31 17:35:36 -0800

    changed drive link

--- src/Navigation.js
+++ src/Navigation.js
@@ -8,7 +8,7 @@ const Nav = () => {
         <div className="navigation">
             <ul>
                 <li className="nav-left"><a href="https://isabeljiang.github.io/portfolio/">Isabel Jiang</a></li>
-                <li className="nav-right"><a href="https://drive.google.com/file/d/1JexbkGeNOBe6yi5Sts1z3YLKKYEiAbtU/view?usp=sharing">cv</a></li>
+                <li className="nav-right"><a href="https://drive.google.com/file/d/1WZ6cQQg-HmEbdTsQ-lbgU81YUTRIGpKC/view?usp=sharing">cv</a></li>
                 <li className="nav-right"><a href="https://facebook.com">about</a></li> 
                 <li className="nav-right"><a href="https://isabeljiang.github.io/portfolio/">work</a></li>
             </ul>
